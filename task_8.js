b03b30fe8b7f44474c05e17aebc10f2b1f72f6fc
Sergey <loveykosergey@ukr.net>
2021-11-09 13:05:53 +0200

    feat: task №8 - Understanding Case Sensitivity in Variables

---
+++ task_8.js
@@ -0,0 +1,10 @@
+// 8. - Understanding Case Sensitivity in Variables
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
