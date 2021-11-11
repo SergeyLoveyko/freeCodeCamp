14b526b56e551cac1bb45e9e6f6134d9746261a2
Sergey <loveykosergey@ukr.net>
2021-11-11 15:24:18 +0200

    feat: task 24 - Compound Assignment With Augmented Division

---
+++ task_24.js
@@ -0,0 +1,14 @@
+// 24. - Compound Assignment With Augmented Division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a);     //   4
+console.log(b);     //  27
+console.log(c);     //   3
