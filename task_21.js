540b10edc441bee7e79d646b8cda8efb35b323f1
Sergey <loveykosergey@ukr.net>
2021-11-11 15:00:57 +0200

    feat: task 21 - Compound Assignment With Augmented Addition

---
+++ task_21.js
@@ -0,0 +1,14 @@
+// 21. - Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+console.log(a);     //  15
+console.log(b);     //  26
+console.log(c);     //  19
