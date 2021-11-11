16b0c2841a8592a6867474ffbe592f77c03581ac
Sergey <loveykosergey@ukr.net>
2021-11-11 15:22:07 +0200

    feat: task 23 - Compound Assignment With Augmented Multiplication

---
+++ task_23.js
@@ -0,0 +1,14 @@
+// 23. - Compound Assignment With Augmented Multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a);     //  25
+console.log(b);     //  36
+console.log(c);     //  46
