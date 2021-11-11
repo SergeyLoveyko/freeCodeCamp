2a24d04dedc2d595b6726deef1a2e513ae0d634c
Sergey <loveykosergey@ukr.net>
2021-11-11 15:03:32 +0200

    feat: task 22 - Compound Assignment With Augmented Subtraction

---
+++ task_22.js
@@ -0,0 +1,14 @@
+// 22. - Compound Assignment With Augmented Subtraction
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a);     //   5
+console.log(b);     //  -6
+console.log(c);     //   2
