73b06d228b2e10a21e1bcec775dd5c7ad4182f2d
Sergey <loveykosergey@ukr.net>
2021-11-12 16:43:26 +0200

    feat: task №27 - Escape Sequences in Strings

---
+++ task_27.js
@@ -0,0 +1,10 @@
+// 27. - Escape Sequences in Strings
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+console.log(myStr);
+
+// FirstLine
+//     \SecondLine
+// ThirdLine
