de90bdb706ae2c05d6c3a46de7ca2b9de5c0339d
Sergey <loveykosergey@ukr.net>
2021-11-12 16:58:26 +0200

    feat: task №31 - Appending Variables to Strings

---
+++ task_31.js
@@ -0,0 +1,8 @@
+//  31. - Appending Variables to Strings
+
+const someAdjective = "fascinating activity";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+console.log(myStr);  //  Learning to code is fascinating activity
