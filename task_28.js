32937da4e59ae146903ddcf82dac675081908901
Sergey <loveykosergey@ukr.net>
2021-11-12 16:46:37 +0200

    feat: task №28 - Concatenating Strings with Plus Operator

---
+++ task_28.js
@@ -0,0 +1,5 @@
+// 28. - Concatenating Strings with Plus Operator
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+console.log(myStr);  //  This is the start. This is the end.
