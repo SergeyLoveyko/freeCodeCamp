a35b6a7d07a52c03ee0da2efdfc7f29f9b090ed1
Sergey <loveykosergey@ukr.net>
2021-11-12 16:50:04 +0200

    feat: task №29 - Concatenating Strings with the Plus Equals Operator

---
+++ task_29.js
@@ -0,0 +1,6 @@
+// 29. - Concatenating Strings with the Plus Equals Operator
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+console.log(myStr);  //  This is the first sentence. This is the second sentence.
