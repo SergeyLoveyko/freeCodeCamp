0c4881d3852a6575d3e1fa7cbda5c29a1579fc31
Sergey <loveykosergey@ukr.net>
2021-11-12 16:54:42 +0200

    feat: task №30 - Constructing Strings with Variables

---
+++ task_30.js
@@ -0,0 +1,6 @@
+//  30. - Constructing Strings with Variables
+
+const myName = "Sergey";
+const myStr = "My name is " + myName + " and I am well!";
+
+console.log(myStr);  //   My name is Sergey and I am well!
