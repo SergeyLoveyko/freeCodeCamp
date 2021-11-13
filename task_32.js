736a4198c6113165c7f5c42ad4caef3e70a68e09
Sergey <loveykosergey@ukr.net>
2021-11-13 16:56:30 +0200

    feat: task №32 - Find the Length of a String

---
+++ task_32.js
@@ -0,0 +1,9 @@
+// 32. - Find the Length of a String
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+console.log(lastNameLength);        //  8
