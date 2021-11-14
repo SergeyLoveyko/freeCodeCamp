356f3bf7869de6ab7a0f8b7c447906790f5e7e02
Sergey <loveykosergey@ukr.net>
2021-11-14 21:59:47 +0200

    feat: task №39 - Store Multiple Values in one Variable using JavaScript Arrays

---
+++ task_39.js
@@ -0,0 +1,9 @@
+//  39. - Store Multiple Values in one Variable using JavaScript Arrays
+
+// Only change code below this line
+const myArray = [];
+
+myArray.push("Hello");
+myArray.push(3);
+
+console.log(myArray);   // myArray[0] - string, myArray[1] - number
