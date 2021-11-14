945b8427d777b9bab453189d59f420485d901755
Sergey <loveykosergey@ukr.net>
2021-11-14 22:04:24 +0200

    feat: task №40 - Nest one Array within Another Array

---
+++ task_40.js
@@ -0,0 +1,9 @@
+//  40. - Nest one Array within Another Array
+
+// Only change code below this line
+const myArray = [];
+
+myArray.push(["Tie", 10]);
+myArray.push(["Hot Dog", 5]);
+
+console.log(myArray);   // arr to arr
