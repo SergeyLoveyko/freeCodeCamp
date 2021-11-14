78780a43e9291fbdd1c76736cd5b5a32aded512f
Sergey <loveykosergey@ukr.net>
2021-11-14 21:55:35 +0200

    feat: task №38 - Word Blanks

---
+++ task_38.js
@@ -0,0 +1,12 @@
+//  38. - Word Blanks
+
+const myNoun = "dog";           //  собака
+const myAdjective = "big";      //  большая
+const myVerb = "ran";           //  бежать
+const myAdverb = "quickly";     //  быстро
+
+// Only change code below this line
+const wordBlanks = "My " + myAdjective + " " + myNoun + " was " + myVerb + " very " + myAdverb + " after the car"; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks);    //  "My big dog was ran very quickly after the car"
