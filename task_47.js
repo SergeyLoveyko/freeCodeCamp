a032584759be5c71bc7fe26386081b0c9a961d4e
Sergey <loveykosergey@ukr.net>
2021-11-15 21:56:43 +0200

    feat: task №47 - Manipulate Arrays With unshift()

---
+++ task_47.js
@@ -0,0 +1,10 @@
+//  47. - Manipulate Arrays With unshift()
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+console.log(myArray);      //  [["Paul", 35], ["dog", 3]]
