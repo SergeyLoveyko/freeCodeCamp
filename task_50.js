c2bf0ea1d8e9534c3c6f662bc0f30d230e4ab1f5
Sergey <loveykosergey@ukr.net>
2021-11-15 23:58:37 +0200

    feat: task №50 - Passing Values to Functions with Arguments

---
+++ task_50.js
@@ -0,0 +1,8 @@
+//  50. - Passing Values to Functions with Arguments
+
+function functionWithArgs(a, b) {
+  return a + b;
+}
+
+console.log(functionWithArgs(1, 2));        //   3
+console.log(functionWithArgs(7, 9));        //  16
