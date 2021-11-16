3bc04578d27355fc7de4b28649c91231f5a6f021
Sergey <loveykosergey@ukr.net>
2021-11-16 19:17:08 +0200

    feat: task №51 - Return a Value from a Function with Return

---
+++ task_51.js
@@ -0,0 +1,9 @@
+//  51. - Return a Value from a Function with Return
+
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(timesFive(5));      //  25
+console.log(timesFive(2));      //  10
+console.log(timesFive(0));      //   0
