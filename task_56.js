7845e084e8f10047381741773a6d076b0582bddf
Sergey <loveykosergey@ukr.net>
2021-11-16 19:46:45 +0200

    feat: task №56 - Assignment with a Returned Value

---
+++ task_56.js
@@ -0,0 +1,13 @@
+//  56. - Assignment with a Returned Value
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+console.log(processed);      //  2 
