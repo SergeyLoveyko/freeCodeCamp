3d1d66bcf26442442a97fb714cad45cbd22bbb41
Sergey <loveykosergey@ukr.net>
2021-11-17 20:16:29 +0200

    feat: task №59 - Use Conditional Logic with If Statements

---
+++ task_59.js
@@ -0,0 +1,15 @@
+//  59. - Use Conditional Logic with If Statements
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+    // Only change code above this line
+
+}
+
+console.log(trueOrFalse(true));         //  true
+console.log(trueOrFalse(false));        //  false
