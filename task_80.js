10851cd561769b4abe0de01c2655a27b58b0db23
Sergey <loveykosergey@ukr.net>
2021-11-20 20:52:57 +0200

    feat: task №80 - Returning Boolean Values from Functions

---
+++ task_80.js
@@ -0,0 +1,10 @@
+//  80. - Returning Boolean Values from Functions
+
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  // Only change code above this line
+}
+
+console.log(isLess(10, 15));    //      true
+console.log(isLess(15, 10));    //      false
