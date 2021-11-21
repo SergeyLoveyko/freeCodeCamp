c140a1ea92e8e62b249d4c8a9e2732296025da35
Sergey <loveykosergey@ukr.net>
2021-11-21 21:22:59 +0200

    feat: task №84 - Accessing Object Properties with Dot Notation

---
+++ task_84.js
@@ -0,0 +1,15 @@
+//  84. - Accessing Object Properties with Dot Notation
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line  //  "ballcap"
+const shirtValue = testObj.shirt;    // Change this line  //  "jersey"
+
+console.log(hatValue);
+console.log(shirtValue);
