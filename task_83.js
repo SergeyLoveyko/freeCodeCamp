85358c299899ab4396226290fb84918ad2bd049a
Sergey <loveykosergey@ukr.net>
2021-11-21 21:17:53 +0200

    feat: task №83 - Build JavaScript Objects

---
+++ task_83.js
@@ -0,0 +1,11 @@
+//  83. - Build JavaScript Objects
+
+const myDog = {
+    // Only change code below this line
+    "name": "Bor",          //  String
+    "legs": 4,              //  number
+    "tails": 1,             //  number
+    "friends": ["people"]   //  arr
+    // Only change code above this line
+};
+
