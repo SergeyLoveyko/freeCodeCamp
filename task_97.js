96853983d3b37abc13ef9eee703e7449dc546bd4
Sergey <loveykosergey@ukr.net>
2021-11-23 19:12:04 +0200

    feat: task №97 - Iterate with JavaScript For Loops

---
+++ task_97.js
@@ -0,0 +1,11 @@
+//  97. - Iterate with JavaScript For Loops
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+
+console.log(myArray);  //  [1, 2, 3, 4, 5]
