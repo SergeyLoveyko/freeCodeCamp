6f72b4e1857cdf69945006ed73a13fc51f76efea
Sergey <loveykosergey@ukr.net>
2021-11-24 19:52:26 +0200

    feat: task №98 - Iterate Odd Numbers With a For Loop

---
+++ task_98.js
@@ -0,0 +1,11 @@
+//  98. - Iterate Odd Numbers With a For Loop
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i += 2) {
+    myArray.push(i);
+}
+
+console.log(myArray);   //  [1, 3, 5, 7, 9]
