ca2dd252c9c92f97c4772a9ea82aa1508817756e
Sergey <loveykosergey@ukr.net>
2021-11-24 19:56:26 +0200

    feat: task №99 - Count Backwards With a For Loop

---
+++ task_99.js
@@ -0,0 +1,11 @@
+//  99. - Count Backwards With a For Loop
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+
+console.log(myArray);   //  [9, 7, 5, 3, 1]
