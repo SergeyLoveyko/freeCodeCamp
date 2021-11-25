8bac3f162d03c1d82d91635874a59b54a62fa1bb
Sergey <loveykosergey@ukr.net>
2021-11-25 22:23:02 +0200

    feat: task №102 - Iterate with JavaScript Do...While Loops

---
+++ task_102.js
@@ -0,0 +1,14 @@
+//  102. - Iterate with JavaScript Do...While Loops
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+    myArray.push(i);
+    i++;
+} while (i < 10);
+
+console.log(myArray);       //  10
+console.log(i);             //  11
