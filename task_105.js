2eaafaa3d33cb6f0275fb95d4124aeb19f559dfb
Sergey <loveykosergey@ukr.net>
2021-11-26 22:40:34 +0200

    feat: task №105 - Generate Random Fractions with JavaScript

---
+++ task_105.js
@@ -0,0 +1,11 @@
+//  105. - Generate Random Fractions with JavaScript
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+
