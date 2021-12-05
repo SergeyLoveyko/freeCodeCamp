de9cd375bf469370ddb5b4dd1ddfe0060519f54f
Sergey <loveykosergey@ukr.net>
2021-12-05 22:59:09 +0200

    feat: task №6 - Default Parameters for Your Functions

---
+++ es6/task_6.js
@@ -0,0 +1,8 @@
+//  6. - Set Default Parameters for Your Functions
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+console.log(increment(5, 2)); //  7
+console.log(increment(5)); //  6
