4bce2105b59d9d54634a7acbf3c2b65b0c9d1eb2
Sergey <loveykosergey@ukr.net>
2021-12-06 23:03:42 +0200

    feat: task №7 - Use the Rest Parameter with Function Parameters

---
+++ es6/task_7.js
@@ -0,0 +1,10 @@
+//  7. - Use the Rest Parameter with Function Parameters
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(0, 1, 2)); //           3
+console.log(sum(1, 2, 3, 4)); //       10
+console.log(sum(5)); //                 5
+console.log(sum()); //                  0
