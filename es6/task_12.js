e24efabb675f978cb860101f7d2d98b9ad26cfba
Sergey <loveykosergey@ukr.net>
2021-12-11 22:50:31 +0200

    feat: task №12 - Use Destructuring Assignment to Assign Variables from Arrays

---
+++ es6/task_12.js
@@ -0,0 +1,7 @@
+//  12. - Use Destructuring Assignment to Assign Variables from Arrays
+
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a];
+
+console.log(a, b);    //    6, 8
