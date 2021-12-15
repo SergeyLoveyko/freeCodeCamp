f2f77464add5b978a545a21ebc6176c5a35ff0e2
Sergey <loveykosergey@ukr.net>
2021-12-15 22:27:46 +0200

    feat: task №16 - Write Concise Object Literal Declarations Using Object Property Shorthand

---
+++ es6/task_16.js
@@ -0,0 +1,15 @@
+//  16. - Write Concise Object Literal Declarations Using Object Property Shorthand
+
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name,
+      age,
+      gender
+    };
+    // Only change code above this line
+  };
+
+console.log("Zodiac Hasbro", 56, "male");
+
+//  name: "Zodiac Hasbro", age: 56, gender: "male"
