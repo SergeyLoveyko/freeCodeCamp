dd4f70970057feae668d0940ab0c3e22d7fd8dc2
Sergey <loveykosergey@ukr.net>
2021-12-22 23:09:32 +0200

    feat: task №23 - Use * to Import Everything from a File

---
+++ es6/task_23.js
@@ -0,0 +1,7 @@
+//  23. - Use * to Import Everything from a File
+
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
