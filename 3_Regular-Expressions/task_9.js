6050751c18431e43e52ef95b3580252aabc11669
Sergey <loveykosergey@ukr.net>
2022-01-05 23:27:08 +0200

    feat: task №9 - Match Letters of the Alphabet

---
+++ 3_Regular-Expressions/task_9.js
@@ -0,0 +1,8 @@
+//  9. - Match Letters of the Alphabet
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+console.log(result);
+console.log(result.length);     //  35
