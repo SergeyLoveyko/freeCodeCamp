496613b01423b5c05be43eca96893b8f6b94398c
Sergey <loveykosergey@ukr.net>
2022-01-07 22:20:37 +0200

    feat: task №11 - Match Single Characters Not Specified

---
+++ 3_Regular-Expressions/task_11.js
@@ -0,0 +1,7 @@
+//  11. - Match Single Characters Not Specified
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^0-9aeiou]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+console.log(result.length);     //  9
