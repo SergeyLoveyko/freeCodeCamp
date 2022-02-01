db893327f00c5f4b38dc23d63339feef67559577
Sergey <loveykosergey@ukr.net>
2022-02-01 22:50:30 +0200

    feat: task deb №2 - Understanding the Differences between the freeCodeCamp and Browser Console

---
+++ 4_Debugging/task_deb_2.js
@@ -0,0 +1,6 @@
+//  2. - Understanding the Differences between the freeCodeCamp and Browser Console
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+console.log(output);    //  "Get this to show once in the freeCodeCamp console and not at all in the browser console"
+console.clear();
