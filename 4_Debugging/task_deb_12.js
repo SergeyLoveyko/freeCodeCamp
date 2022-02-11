0ff8f486b96cc90f037e00168040646348006be0
Sergey <loveykosergey@ukr.net>
2022-02-11 23:02:56 +0200

    feat: task deb №12 - Prevent Infinite Loops with a Valid Terminal Condition

---
+++ 4_Debugging/task_deb_12.js
@@ -0,0 +1,9 @@
+//  12. - Prevent Infinite Loops with a Valid Terminal Condition
+
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+}
+
+myFunc();   //  Still going!
