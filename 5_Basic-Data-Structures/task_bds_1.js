093cce9a25ecb9b19e0163ab73d6e7d6b4fe9914
Sergey <loveykosergey@ukr.net>
2022-02-12 22:34:03 +0200

    feat: task bds №1 - Use an Array to Store a Collection of Data

---
+++ 5_Basic-Data-Structures/task_bds_1.js
@@ -0,0 +1,6 @@
+//  1. - Use an Array to Store a Collection of Data
+
+let yourArray = ['string', 6, true, false, {}]; // Change this line
+//  Your array should contain at least one string, one number, and one boolean.
+
+console.log(yourArray);     //  [ 'string', 6, true, false, {} ]
