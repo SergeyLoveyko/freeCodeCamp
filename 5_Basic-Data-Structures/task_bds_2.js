efe6d63977c4e36bcb8d2fc19ab236eda6e2ad87
Sergey <loveykosergey@ukr.net>
2022-02-13 21:58:21 +0200

    feat: task bds №2 - Access an Array's Contents Using Bracket Notation

---
+++ 5_Basic-Data-Structures/task_bds_2.js
@@ -0,0 +1,8 @@
+//  2. - Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "notebook";
+// Only change code above this line
+console.log(myArray);   //     
+//      myArray[1] should not be equal to the letter b
