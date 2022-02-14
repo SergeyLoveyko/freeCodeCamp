f345dcebb78f61838e09f3d9d00ca21202b34bc5
Sergey <loveykosergey@ukr.net>
2022-02-14 16:49:22 +0200

    feat: task bds №3 - Add Items to an Array with push() and unshift()

---
+++ 5_Basic-Data-Structures/task_bds_3.js
@@ -0,0 +1,12 @@
+//  3. - Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift("I", 2, "three");
+    arr.push(7, "VIII", 9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));    //
+//  should now return ["I", 2, "three", "IV", 5, "six", 7, "VIII", 9]
