a1d440ec40fcb106a33a5ac13baa3ce5faad368d
Sergey <loveykosergey@ukr.net>
2022-02-19 18:43:28 +0200

    feat: task bds №8 - Copy an Array with the Spread Operator

---
+++ 5_Basic-Data-Structures/task_bds_8.js
@@ -0,0 +1,17 @@
+//  8. - Copy an Array with the Spread Operator
+
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+        newArr.push([...arr]);
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+console.log(copyMachine([1, 2, 3], 5));
+console.log(copyMachine([true, true, null], 1));
+console.log(copyMachine(["it works"], 3));
