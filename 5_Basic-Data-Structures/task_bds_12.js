8a1b674b5b3a1b1c34a38430a816049634b667c9
Sergey <loveykosergey@ukr.net>
2022-02-23 22:46:06 +0200

    feat: task bds №12 - Create complex multi-dimensional arrays

---
+++ 5_Basic-Data-Structures/task_bds_12.js
@@ -0,0 +1,11 @@
+//  12. - Create complex multi-dimensional arrays
+
+let myNestedArray = [
+    // Only change code below this line
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    ['concat', false, true, 'spread', 'array', ['deep']],
+    ['mutate', 1327.98, 'splice', 'slice', 'push',['paper', ['deeper']]],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', ['remember', [['deepest'], 'three']]]
+    // Only change code above this line
+];
