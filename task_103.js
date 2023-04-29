b74ef11334ef6e5968a5dafc820f71ec6c3a44be
Sergey <loveykosergey@ukr.net>
2023-04-30 01:19:39 +0300

    feat: task №103 - replace loops using recursion

--- task_103.js
+++ task_103.js
@@ -1,11 +1,30 @@
 //  103. - Replace Loops using Recursion
 
+// let total = 0;
+
 function sum(arr, n) {
   // Only change code below this line
+
+  // if (arr[n - 1] == undefined) {
+  //   return  total;
+  // } else {
+  //   total += arr[n - 1];
+  //   n--;
+  //   sum(arr, n);
+  // }
+
+  // return total;
+
+  
+    if (arr[n - 1] == undefined) {
+    return 0;
+  } else {
+    return arr[n - 1] + sum(arr, n - 1);
+  }
 
   // Only change code above this line
 }
 
-console.log([1], 0);                //  0
-console.log([2, 3, 4], 1);          //  2
-console.log([2, 3, 4, 5], 3);       //  9
+console.log(sum([1], 0));                //  0
+console.log(sum([2, 3, 4], 1));          //  2
+console.log(sum([2, 3, 4, 5], 3));       //  9
