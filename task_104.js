5c69be44307cbcf226238895287046fcd5b9d736
Sergey <loveykosergey@ukr.net>
2023-04-30 03:00:03 +0300

    feat: task №104 - profile lookup

--- task_104.js
+++ task_104.js
@@ -31,12 +31,26 @@ const contacts = [
 function lookUpProfile(name, prop) {
   // Only change code below this line
 
+  for (let i = 0; i < contacts.length; i++) {
+
+    if (contacts[i].firstName == name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return 'No such property';
+      }
+    } 
+  
+  }
+
+  return 'No such contact';
+
   // Only change code above this line
 }
 
 console.log(lookUpProfile("Kristian", "lastName"));   //  Vos
 console.log(lookUpProfile("Sherlock", "likes"));      //  ["Intriguing Cases", "Violin"]
-console.log(lookUpProfile("Harry", "likes"));         //  No such contact
+console.log(lookUpProfile("Harry", "likes"));         //  should return an array
 console.log(lookUpProfile("Bob", "number"));          //  No such contact
 console.log(lookUpProfile("Bob", "potato"));          //  No such property
-console.log(lookUpProfile("Akira", "likes"));         //  
+console.log(lookUpProfile("Akira", "address"));         //  
