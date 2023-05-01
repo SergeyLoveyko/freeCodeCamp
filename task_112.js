259b33915441ace0f1f7f6cdf549930ac73ddc9e
Sergey <loveykosergey@ukr.net>
2023-05-01 03:53:32 +0300

    feat: task №112 - use recursion to create a countdown

--- task_112.js
+++ task_112.js
@@ -2,6 +2,18 @@
 
 // Only change code below this line
 function countdown(n){
-  return;
+
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+
 }
 // Only change code above this line
+
+console.log( countdown(-1) );       //  return []
+console.log( countdown(10) );       //  return [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+console.log( countdown(5) );       //  return [5, 4, 3, 2, 1]
