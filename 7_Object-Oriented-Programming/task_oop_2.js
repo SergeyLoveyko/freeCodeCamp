76b6699949e5d161d79ff6d122967319bc80ad6c
Sergey <loveykosergey@ukr.net>
2024-05-03 03:21:09 +0300

    feat: task oop №2 - Use Dot Notation to Access the Properties of an Object

---
+++ 7_Object-Oriented-Programming/task_oop_2.js
@@ -0,0 +1,10 @@
+//  2. - Use Dot Notation to Access the Properties of an Object
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Only change code below this line
+
+console.log( dog.name );        //  Spot
+console.log( dog.numLegs );     //  4
