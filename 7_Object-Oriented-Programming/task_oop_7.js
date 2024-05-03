2d65bcfbaa9c1c7345047b28e6b4623c9123de39
Sergey <loveykosergey@ukr.net>
2024-05-03 03:54:15 +0300

    feat: task oop №7 - Extend Constructors to Receive Arguments

---
+++ 7_Object-Oriented-Programming/task_oop_7.js
@@ -0,0 +1,13 @@
+//  7. - Extend Constructors to Receive Arguments
+
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+
+// Only change code below this line
+
+let terrier = new Dog("Samy", "black");
+
+console.log(terrier)
