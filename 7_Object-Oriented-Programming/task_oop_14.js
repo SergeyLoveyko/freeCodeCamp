01f95734e0ee0f417ed643f131409672f21eac5f
Sergey <loveykosergey@ukr.net>
2024-05-03 19:39:21 +0300

    feat: task oop №14 - Remember to Set the Constructor Property when Changing the Prototype

---
+++ 7_Object-Oriented-Programming/task_oop_14.js
@@ -0,0 +1,17 @@
+//  14. - Remember to Set the Constructor Property when Changing the Prototype
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor: Dog,     //  here add
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
