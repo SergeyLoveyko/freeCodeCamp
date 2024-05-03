c63a2297419ef6e22b843631e82e6d7420ca071d
Sergey <loveykosergey@ukr.net>
2024-05-03 17:35:48 +0300

    feat: task oop №13 - Change the Prototype to a New Object

---
+++ 7_Object-Oriented-Programming/task_oop_13.js
@@ -0,0 +1,16 @@
+//  13. - Change the Prototype to a New Object
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function() {
+    console.log( "nom nom nom" );
+  },
+  describe: function() {
+    console.log( "My name is " + this.name );
+  }
+};
